047090329e242ffaa7aa0c98b40fd75e944d5915
Vectorfield4 <vectorfield4@gmail.com>
2025-07-19 22:58:03 +0300

    fix graphql

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { initDataRaw } from '@telegram-apps/sdk-react';
 import { lastErrorStore } from '@/store/mainScreenStore';
 
-const graphqlUri = import.meta.env.VITE_BACKEND_API || 'http://localhost:8080/graphql';
+const graphqlUri = (import.meta.env.VITE_BACKEND_API || 'http://localhost:8080') + '/graphql';
 const httpLink = createHttpLink({
   uri: graphqlUri,
   fetch: async (uri, options) => {
@@ -22,7 +22,7 @@ const httpLink = createHttpLink({
     try {
       return await fetch(uri, { ...options, headers });
     } catch (err) {
-      setLastError(`URL: ${graphqlUri}`);
+      setLastError(`Error: ${err}`);
       throw err;
     }
   },
